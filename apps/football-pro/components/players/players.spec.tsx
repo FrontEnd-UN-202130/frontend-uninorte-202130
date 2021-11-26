2ca37a565c0ad65f9cbfab02153f07cdcdb750eb
Juan <jbojato@uninorte.edu.co>
2021-11-25 22:52:36 -0500

    test: added test to players

--- apps/football-pro/components/players/players.spec.tsx
+++ apps/football-pro/components/players/players.spec.tsx
@@ -1,10 +1,12 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Players from './players';
 
 describe('Players', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Players />);
+  it('should render successfully', async () => {
+    const { baseElement } = render(<Players teamid={434} />);
+    const morales = await screen.findByRole('gridcell', { name: 'L. Morales' });
+    expect(morales).toBeInTheDocument();
     expect(baseElement).toBeTruthy();
   });
 });
