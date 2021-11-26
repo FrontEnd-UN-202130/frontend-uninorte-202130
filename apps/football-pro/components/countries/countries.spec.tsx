7da6bdaf6629011f8c47b03330b2e14eb573244a
Juan <jbojato@uninorte.edu.co>
2021-11-25 23:23:09 -0500

    test: added test to countries.

--- apps/football-pro/components/countries/countries.spec.tsx
+++ apps/football-pro/components/countries/countries.spec.tsx
@@ -1,10 +1,14 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Countries from './countries';
 
 describe('Countries', () => {
-  it('should render successfully', () => {
+  it('should render successfully', async () => {
     const { baseElement } = render(<Countries />);
+    const austria = await screen.findByText(/austria/i);
+    const argentina = await screen.findByText(/argentina/i);
+    expect(austria).toBeInTheDocument();
+    expect(argentina).toBeInTheDocument();
     expect(baseElement).toBeTruthy();
   });
 });
